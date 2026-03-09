a8dfe7442d0fb1571121d6ebd1bf7a829299b491
Aashutosh Pandey <aashutoshpandey011@gmail.com>
2026-03-09 10:51:04 +0545

    added models task.py

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Interger, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from app.core.database import Base
 
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Interger, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
     completed = Column(Boolean, default=False)
