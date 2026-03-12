f9c0d5cbc9c10eec4695530c7258b2cede28d5eb
Aashutosh Pandey <aashutoshpandey011@gmail.com>
2026-03-12 21:39:42 +0545

    updated App code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./components/forms/login";
 import Register from "./components/forms/register";
 
 function App() {
-  return <Login />;
-  return <Register />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
