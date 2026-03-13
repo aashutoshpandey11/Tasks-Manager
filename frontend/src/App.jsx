e2090cd2c16a5b316694d4b4536336813546ad47
Aashutosh Pandey <aashutoshpandey011@gmail.com>
2026-03-13 22:00:20 +0545

    Update app code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./components/forms/login";
 import Register from "./components/forms/register";
+import Dashboard from "./components/dashboard/userdashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
