3587506475255230d65677daf139f78ef0e66f0a
Aashutosh Pandey <aashutoshpandey011@gmail.com>
2026-03-15 00:19:09 +0545

    updated routes tasks

--- backend/app/api/routes/tasks.py
+++ backend/app/api/routes/tasks.py
@@ -7,7 +7,7 @@ from app.crud import task as task_crud
 
 router = APIRouter()
 
-# Dependency
+# DB Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -15,10 +15,22 @@ def get_db():
     finally:
         db.close()
 
+# CREATE
 @router.post("/", response_model=TaskResponse)
 def create(task: TaskCreate, db: Session = Depends(get_db)):
     return task_crud.create_task(db, task)
 
-@router.get("/")
+# GET ALL
+@router.get("/", response_model=list[TaskResponse])
 def get_all(db: Session = Depends(get_db)):
     return task_crud.get_tasks(db)
+
+# DELETE
+@router.delete("/{task_id}")
+def delete(task_id: int, db: Session = Depends(get_db)):
+    return task_crud.delete_task(db, task_id)
+
+# UPDATE
+@router.put("/{task_id}", response_model=TaskResponse)
+def update(task_id: int, task: TaskCreate, db: Session = Depends(get_db)):
+    return task_crud.update_task(db, task_id, task)
