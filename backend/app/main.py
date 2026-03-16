4f08718896aa7960d8386d72d67c0f5a57a561d3
Aashutosh Pandey <aashutoshpandey011@gmail.com>
2026-03-17 00:27:27 +0545

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.database import engine, Base
 from app.api.routes import tasks
-from app.core.database import Base, engine
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#Create tables
-Base.metadata.create_all(bind=engine)
+# CORS (for React)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# Routes
 app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
+
+# Root
+@app.get("/")
+def root():
+    return {"message": "API is running 🚀"}
